634b9fde45a0681805db973aaf89f4cd0b62eca2
Krishnakant Shedge <shedgekrishnakant33@gmail.com>
2021-10-25 19:07:40 +0200

    Calculator: Initial code

---
+++ README.md
@@ -0,0 +1,7 @@
+Angular Material In Calculator App
+
+Consider the calculator application that you have built in your laboratory work 6. Apply at least following Angular Material components in your calculator application:
+1. Toolbar: https://material.angular.io/components/toolbar/overview
+2. Card: https://material.angular.io/components/card/overview
+3. Button: https://material.angular.io/components/button/overview
+4. Icon: https://material.angular.io/components/icon/overview
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
